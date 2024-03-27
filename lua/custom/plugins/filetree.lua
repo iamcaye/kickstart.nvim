17ffecd8cc40dcc9409990dd084708557effa99d
iamcaye <cayetano.biehler@gmail.com>
2024-03-27 19:57:25 +0100

    tabstop and remove filetree

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -1,13 +1,14 @@
-return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  config = function()
-    require('neo-tree').setup {}
-    vim.keymap.set('n', '<leader>nt', '<cmd>Neotree toggle<cr>')
-  end,
-}
+return {}
+-- return {
+--   'nvim-neo-tree/neo-tree.nvim',
+--   version = '*',
+--   dependencies = {
+--     'nvim-lua/plenary.nvim',
+--     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+--     'MunifTanjim/nui.nvim',
+--   },
+--   config = function()
+--     require('neo-tree').setup {}
+--     vim.keymap.set('n', '<leader>nt', '<cmd>Neotree toggle<cr>')
+--   end,
+-- }
